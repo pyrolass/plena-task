c29fa1a253867fffe0ba21be331e8e09e77b9b9c
pyrolass <las.suleman1@gmail.com>
2024-06-19 20:16:14 +0300

    redis interceptor added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), UserModule, RedisModule],
 })
 export class AppModule {}
