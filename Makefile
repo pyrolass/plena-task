482feea1d36a69e65f881b16bf42d2c4b98e03a4
pyrolass <las.suleman1@gmail.com>
2024-06-19 15:03:40 +0300

    make file changes

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 .DEFAULT_GOAL := run
 
+
+docker-compose:
+	docker-compose up -d
+	
 run:
 	yarn run start:dev
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
